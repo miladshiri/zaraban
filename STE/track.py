5d79bdabc19d1e8e940d8cfef200a5aba0c1a68b
milad <miladonlinemail@gmail.com>
2019-04-07 12:42:16 +0430

    Add and test track_point function
    - rename track_single_eulerian
    - add track_point  to track a point based on lagrangian

--- STE/track.py
+++ STE/track.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ############################################
 ###                 Zaraban              ### 
 ###             A python tools for       ###
@@ -13,7 +14,7 @@ from matplotlib import pyplot as plt
 
 
 
-def track_single(im1, im2, WS, SS):
+def track_single_eulerian(im1, im2, WS, SS):
     ######
     ### track_single > applying block matching between two frames
     ###                to estimate speckles' displacement
@@ -34,9 +35,9 @@ def track_single(im1, im2, WS, SS):
     f_rows = f_size[0]
     f_cols = f_size[1]
 
-    PD = WS + SS #Paddin
+    PD = WS + SS #Padding
     
-    TH = 250  #Speckle Threshold
+    TH = 0  #Speckle Threshold
     
     vectx = np.zeros(f_size)
     vecty = np.zeros(f_size)
@@ -46,8 +47,7 @@ def track_single(im1, im2, WS, SS):
             progress += 1
             print('{:.3}'.format(progress/((f_rows-2*PD)*(f_cols-2*PD))*100))
             window = im1[row-WS:row+WS,col-WS:col+WS] 
-            if np.mean(window) > 0:
-    #            print ("row:{}, col:{}".format(row, col))
+            if np.mean(window) > TH:
                 match_score = np.zeros((2*SS+1, 2*SS+1))
                 cross_col = np.zeros((2*SS+1, 2*SS+1))
     
@@ -55,13 +55,13 @@ def track_single(im1, im2, WS, SS):
                     for jj in range(-SS, SS+1):
                         patch = im2[row+ii-WS:row+ii+WS, col+jj-WS:col+jj+WS]
     #                    match_score[ii+SS, jj+SS] = np.sum(np.power(window-patch, 2))            
-                        cross_col[ii+SS, jj+SS] = np.sum((window-window.mean())*(patch-patch.mean())) / (window.std()*patch.std())
+                        cross_col[ii+SS, jj+SS] = abs(np.mean((window-window.mean())*(patch-patch.mean()) / (window.std()*patch.std())))
                         match_score = cross_col
-                
-                match_score = np.nan_to_num(match_score)      
-                if match_score[SS, SS] == 0:
-                   vectx[row, col] = 0;
-                   vecty[row, col] = 0; 
+                        print (cross_col[ii+SS, jj+SS])
+                match_score = np.nan_to_num(match_score)
+                if match_score[SS, SS] == 1 or match_score[SS, SS] != match_score[SS, SS]:
+                       vectx[row, col] = 0;
+                       vecty[row, col] = 0; 
                 else:
                     a, b = np.where(match_score == np.max(match_score))
                     vectx[row, col] = a[0] - (SS)
@@ -69,8 +69,68 @@ def track_single(im1, im2, WS, SS):
          
     return vectx, vecty
     
-            
+
         
+def track_point(im1, im2, markers, WS, SS):
+    ######
+    ### track_point > applying block matching to track a point between 
+    ###               two frames.
+    ### im1 = firt frame
+    ### im2 = second frame
+    ### markers = inpute initial points
+    ### WS = Window Size
+    ### SS= Search Size
+    #######
+    
+    if type(im1) != np.ndarray:
+           print ('Error: Input image1 should be numpy.ndarray')
+           return 0
+    if type(im2) != np.ndarray:
+           print ('Error: Input image2 should be numpy.ndarray')
+           return 0
+       
+    f_size = im1.shape
+    f_rows = f_size[0]
+    f_cols = f_size[1]
+    counts = markers.shape[0]
+    PD = WS + SS #Padding
+    
+    TH = 0  #Speckle Threshold
+    
+    vectx = np.zeros(f_size)
+    vecty = np.zeros(f_size)
+    progress = 0
+    displacements = markers.copy()
+    for i in range(0, counts):
+        marker = markers[i]
+        progress += 1
+        print('{:.3}'.format(progress/(counts)*100))
+        col = marker[0]
+        row = marker[1]
+        window = im1[row-WS:row+WS,col-WS:col+WS] 
+        if np.mean(window) > TH:
+            match_score = np.zeros((2*SS+1, 2*SS+1))
+            cross_col = np.zeros((2*SS+1, 2*SS+1))
+
+            for ii in range(-SS, SS+1):
+                for jj in range(-SS, SS+1):
+                    patch = im2[row+ii-WS:row+ii+WS, col+jj-WS:col+jj+WS]
+#                    match_score[ii+SS, jj+SS] = np.sum(np.power(window-patch, 2))            
+                    cross_col[ii+SS, jj+SS] = abs(np.mean((window-window.mean())*(patch-patch.mean()) / (window.std()*patch.std())))
+                    match_score = cross_col
+                    print (cross_col[ii+SS, jj+SS])
+            match_score = np.nan_to_num(match_score)
+            if match_score[SS, SS] == 1 or match_score[SS, SS] != match_score[SS, SS]:
+               displacements[i, 0] = 0;
+               displacements[i, 1] = 0;
+            else:
+                a, b = np.where(match_score == np.max(match_score))
+                displacements[i, 0] = a[0] - (SS)
+                displacements[i, 1] = b[0] - (SS)
+     
+    return displacements + markers
+    
+               
         
         
         
--- test_track.py
+++ test_track.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ############################################
 ###                 Zaraban              ### 
 ###             A python tools for       ###
@@ -12,9 +13,15 @@ from PIL import Image
 import cv2
 from matplotlib import pyplot as plt
 
-path = "E:/MathLab/EWI_Project/Database/Behnam/4C/original/"
-im1 = Image.open(path + "im (4).bmp")
-im2 = Image.open(path + "im (5).bmp")
+#path = "E:/MathLab/EWI_Project/Database/Behnam/4C/original/"
+path = "E:/MathLab/Python/Projects/Zaraban_project/source/track_object/test1/"
+
+#im1 = Image.open(path + "im (4).bmp")
+#im2 = Image.open(path + "im (5).bmp")
+
+im1 = Image.open(path + "1.jpg")
+im2 = Image.open(path + "2.jpg")
+
 
 im1 = im1.resize((100, 100))
 im2 = im2.resize((100, 100))
@@ -25,39 +32,45 @@ im2 = np.array(im2)
 plt.imshow(im1)
     
 
-WS = 4 #window_size
-SS = 2 #search_size
+WS = 5 #window_size
+SS = 30 #search_size
 
 
-(vectx, vecty) = track.track_single(im1=im1, im2=im2, WS=WS, SS=SS)
+markers = np.array([[39, 39],])
+new_markers = track.track_point(im1=im1, im2=im2, markers=markers, WS=WS, SS=SS)
 
-
-imx = np.floor((vectx + SS)*255/8)
-imy = np.floor((vecty + SS)*255/8)
-
-plt.imshow(imx, cmap='plasma')
+plt.imshow(im1)
+plt.plot([markers[0, 0], new_markers[0, 0]], [markers[0, 1], new_markers[0, 1]])
 plt.show()
-plt.imshow(imy, cmap='plasma')        
-plt.show()        
-
-
-
-imx_color = cv2.applyColorMap(imx.astype(np.uint8), cv2.COLORMAP_RAINBOW)
-imx_color = cv2.blur(imx_color, (5, 5))
-imy_color = cv2.applyColorMap(imy.astype(np.uint8), cv2.COLORMAP_RAINBOW)
-
-image_gray = imx_color.copy()
-image_gray[:, :, 0] = im2
-image_gray[:, :, 1] = im2
-image_gray[:, :, 2] = im2
-
-
-
-im1_ov = cv2.addWeighted(imx_color, .5, image_gray, .5, 0)
-
-plt.imshow(im1_ov)   
-plt.show()   
-plt.imshow(imy_color)        
-plt.show()
-    
+#(vectx, vecty) = track.track_single_eulerian(im1=im1, im2=im2, WS=WS, SS=SS)
+#
+#
+#imx = np.floor((vectx + SS)*255/8)
+#imy = np.floor((vecty + SS)*255/8)
+#
+#plt.imshow(imx, cmap='plasma')
+#plt.show()
+#plt.imshow(imy, cmap='plasma')        
+#plt.show()        
+#
+#
+#
+#imx_color = cv2.applyColorMap(imx.astype(np.uint8), cv2.COLORMAP_RAINBOW)
+#imx_color = cv2.blur(imx_color, (5, 5))
+#imy_color = cv2.applyColorMap(imy.astype(np.uint8), cv2.COLORMAP_RAINBOW)
+#
+#image_gray = imx_color.copy()
+#image_gray[:, :, 0] = im2
+#image_gray[:, :, 1] = im2
+#image_gray[:, :, 2] = im2
+#
+#
+#
+#im1_ov = cv2.addWeighted(imx_color, .5, image_gray, .5, 0)
+#
+#plt.imshow(im1_ov)   
+#plt.show()   
+#plt.imshow(imy_color)        
+#plt.show()
+#    
 
