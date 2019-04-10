9964252ec00a9a7c8b5687830d2428f3d5521e56
milad <miladonlinemail@gmail.com>
2019-04-11 03:00:34 +0430

    use multiple images for speckle detection

--- STE/speckle.py
+++ STE/speckle.py
@@ -26,24 +26,31 @@ im2 = im2.resize((200, 200))
 
 im1 = np.array(im1)
 im2 = np.array(im2)
-
 f_size = im1.shape
+
+im_list = im1.reshape(1, f_size[0], f_size[1])
+im_list = np.concatenate((im_list, im2.reshape(1, f_size[0], f_size[1])), axis=0)
+
 
 samples_num = 400
 kernel_width = 5
+#samples_num*im_list.shape[0]
+patches = np.empty((0, kernel_width*2+1, kernel_width*2+1))
 
-X = np.random.randint(1+kernel_width, f_size[1]-kernel_width, samples_num)
-Y = np.random.randint(1+kernel_width, f_size[0]-kernel_width, samples_num)
+for im in im_list:
 
-patches = np.zeros((samples_num, kernel_width*2+1, kernel_width*2+1))
-
-for num in range(1, samples_num):
-    patches[num, :, :] = im1[Y[num]-kernel_width:Y[num]+kernel_width+1, X[num]-kernel_width:X[num]+kernel_width+1]
+    X = np.random.randint(1+kernel_width, f_size[1]-kernel_width, samples_num)
+    Y = np.random.randint(1+kernel_width, f_size[0]-kernel_width, samples_num)
     
+    
+    for num in range(1, samples_num):
+        patch = im[Y[num]-kernel_width:Y[num]+kernel_width+1, X[num]-kernel_width:X[num]+kernel_width+1]
+        patches = np.concatenate((patches, patch.reshape((1, patch.shape[0], patch.shape[1]))), axis=0)
+        
 #patches = image.extract_patches_2d(im1, (sample_window, sample_window), max_patches=samples_num, random_state=np.random)
-patches = patches.reshape(samples_num, -1)
+patches = patches.reshape(patches.shape[0], -1)
 
-features = np.zeros((samples_num, 5))
+features = np.zeros((patches.shape[0], 5))
 
 features[:, 0] = patches.mean(axis=1)
 features[:, 1] = patches.std(axis=1)
@@ -62,11 +69,35 @@ if centers[0, 4] > centers[1, 4]:
 else:
     speckle_class = 1
 
-speckles_x = X[model.labels_==speckle_class]
-speckles_y = Y[model.labels_==speckle_class]
 
-hole_x = X[model.labels_==1-speckle_class]
-hole_y = Y[model.labels_==1-speckle_class]
+#####Use model
+
+
+X = np.random.randint(1+kernel_width, f_size[1]-kernel_width, samples_num)
+Y = np.random.randint(1+kernel_width, f_size[0]-kernel_width, samples_num)
+
+patches = np.zeros((samples_num, kernel_width*2+1, kernel_width*2+1))
+
+for num in range(1, samples_num):
+    patches[num, :, :] = im1[Y[num]-kernel_width:Y[num]+kernel_width+1, X[num]-kernel_width:X[num]+kernel_width+1]
+
+patches = patches.reshape(patches.shape[0], -1)
+
+features = np.zeros((patches.shape[0], 5))
+
+features[:, 0] = patches.mean(axis=1)
+features[:, 1] = patches.std(axis=1)
+features[:, 2] = patches.max(axis=1)
+features[:, 3] = patches.min(axis=1)
+features[:, 4] = patches.sum(axis=1)
+
+labels = model.predict(features)
+
+speckles_x = X[labels==speckle_class]
+speckles_y = Y[labels==speckle_class]
+
+hole_x = X[labels==1-speckle_class]
+hole_y = Y[labels==1-speckle_class]
 
 
 imn = im1.copy()
