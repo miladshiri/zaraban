fe6b195c52714d54239bdb672c808991f5bb6413
milad <miladonlinemail@gmail.com>
2019-04-12 00:19:42 +0430

    Completing speckle detection class
    including: supervised and unsupervised speckle detection

--- STE/speckle.py
+++ STE/speckle.py
@@ -13,128 +13,146 @@ import cv2
 from matplotlib import pyplot as plt
 from sklearn.feature_extraction import image
 from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def pick_point(frame, points_size=100, title=None):      
+    fig = plt.figure()
+    plt.imshow(frame)
+    plt.title(title)
+    points = plt.ginput(points_size)
+    plt.close(fig)
+    X = []
+    Y = []
+    for point in points:
+        X.append(int(round(point[0])))
+        Y.append(int(round(point[1])))
+    
+    return np.array(X), np.array(Y)
+
+
+def patch_extraction(frame, points, kernel_radius):
+    X = points[0]
+    Y = points[1]
+    samples_num = X.shape[0]
+    patches = np.zeros((samples_num, kernel_radius*2+1, kernel_radius*2+1))
+    
+    for num in range(0, samples_num):
+        patches[num, :, :] = frame[Y[num]-kernel_radius:Y[num]+kernel_radius+1, X[num]-kernel_radius:X[num]+kernel_radius+1]
+    return patches
+
+
+def feature_extraction(patches):
+    patches = patches.reshape(patches.shape[0], -1)
+    features = np.zeros((patches.shape[0], 5))
+    
+    features[:, 0] = patches.mean(axis=1)
+    features[:, 1] = patches.std(axis=1)
+    features[:, 2] = patches.max(axis=1)
+    features[:, 3] = patches.min(axis=1)
+    features[:, 4] = patches.sum(axis=1)
+    return features
+
+
+def random_patches(frames, kernel_radius, samples_num=10):
+    patches = np.empty((0, kernel_radius*2+1, kernel_radius*2+1))
+    f_size = frames.shape[1:]
+    for im in frames:
+        X = np.random.randint(1+kernel_radius, f_size[1]-kernel_radius, samples_num)
+        Y = np.random.randint(1+kernel_radius, f_size[0]-kernel_radius, samples_num)
+        patches = np.concatenate((patches, patch_extraction(im, (X, Y), kernel_radius)), axis=0)
+    return patches
+
+
+def train_test_feature_select(frames, points_size, kernel_radius):
+    features1 = np.array([])
+    features2 = np.array([])
+
+    for frame in frames:
+        title = 'Please select {} points with speckle pattern...'.format(points_size)
+        X, Y = pick_point(frame, points_size=points_size, title=title)
+        patches = patch_extraction(frame, (X, Y), kernel_radius)
+        features1 = np.concatenate((features1, feature_extraction(patches)), axis=0) if features1.size else feature_extraction(patches)
+    labels1 = np.ones((features1.shape[0], 1))
+
+    for frame in frames:
+        title = 'Please select {} points with non-speckle pattern...'.format(points_size)
+        X, Y = pick_point(frame, points_size=points_size, title=title)
+        patches = patch_extraction(frame, (X, Y), kernel_radius)
+        features2 = np.concatenate((features2, feature_extraction(patches)), axis=0) if features2.size else feature_extraction(patches)
+    labels2 = np.zeros((features2.shape[0], 1))
+
+    labels = np.concatenate((labels1, labels2), axis=0)
+    features = np.concatenate((features1, features2), axis=0)
+    data = np.concatenate((features, labels), axis=1)
+    np.random.shuffle(data)
+    return data[:, :-1], data[:, -1]
 
 
 class Speckle:
-    def __init__(self, sample_size=100, kernel_radius=5):
+    def __init__(self, sample_size=100, kernel_radius=5, method='unsupervised'):
         self.sample_size = sample_size
         self.kernel_radius = kernel_radius
-
-    def fit(self, im_list):
-        kernel_width = self.kernel_radius
-        samples_num = self.sample_size
+        self.method = method
         
-        patches = np.empty((0, kernel_width*2+1, kernel_width*2+1))
+    def fit(self, features, labels=None):
+#        kernel_width = self.kernel_radius
+#        samples_num = self.sample_size
+#        f_size = im_list.shape[1:]
         
-        for im in im_list:
-        
-            X = np.random.randint(1+kernel_width, f_size[1]-kernel_width, samples_num)
-            Y = np.random.randint(1+kernel_width, f_size[0]-kernel_width, samples_num)
+        if self.method == 'unsupervised':
+            model = KMeans(n_clusters=2)
+            model.fit(features[:, :1])
             
-            
-            for num in range(1, samples_num):
-                patch = im[Y[num]-kernel_width:Y[num]+kernel_width+1, X[num]-kernel_width:X[num]+kernel_width+1]
-                patches = np.concatenate((patches, patch.reshape((1, patch.shape[0], patch.shape[1]))), axis=0)
+            centers = model.cluster_centers_
+            if centers[0, 0] > centers[1, 0]:
+                speckle_class = 0
+            else:
+                speckle_class = 1
                 
-        patches = patches.reshape(patches.shape[0], -1)
-        
-        features = np.zeros((patches.shape[0], 5))
-        
-        features[:, 0] = patches.mean(axis=1)
-        features[:, 1] = patches.std(axis=1)
-        features[:, 2] = patches.max(axis=1)
-        features[:, 3] = patches.min(axis=1)
-        features[:, 4] = patches.sum(axis=1)
-                
-        model = KMeans(n_clusters=2)
-        model.fit(features[:, :1])
-        
-        centers = model.cluster_centers_
-        if centers[0, 0] > centers[1, 0]:
-            speckle_class = 0
-        else:
+        elif self.method == 'supervised':
+            model = KNeighborsClassifier(n_neighbors=3)
+            model.fit(features, labels)
             speckle_class = 1
-        
+            
         self.__model = model
-        self.__speckle_class = speckle_class
+        self.__speckle_class = speckle_class    
+        return 1
     
-    def predict(self, patches, visualize=False, image=None):
-        patches = patches.reshape(patches.shape[0], -1)
-        features = np.zeros((patches.shape[0], 5))
-        
-        features[:, 0] = patches.mean(axis=1)
-        features[:, 1] = patches.std(axis=1)
-        features[:, 2] = patches.max(axis=1)
-        features[:, 3] = patches.min(axis=1)
-        features[:, 4] = patches.sum(axis=1)
-        model = self.__model
-        labels = model.predict(features[:, :1])
-        if self.__speckle_class == 0:
-            labels = np.ones_like(labels) - labels
-        
-        if visualize == True:
-            speckles_x = X[labels==1]
-            speckles_y = Y[labels==1]     
-            hole_x = X[labels==0]
-            hole_y = Y[labels==0]
-            
-            im = image.copy()
-            for i in range(1, speckles_x.shape[0]):
-                imn = cv2.rectangle(im, (speckles_x[i]-kernel_width, speckles_y[i]-kernel_width)
-                , (speckles_x[i]+kernel_width, speckles_y[i]+kernel_width), (200, 200, 230), thickness=1)
-            
-            for i in range(1, hole_x.shape[0]):
-                imn = cv2.rectangle(im, (hole_x[i]-kernel_width, hole_y[i]-kernel_width)
-                , (hole_x[i]+kernel_width, hole_y[i]+kernel_width), (100, 100, 50), thickness=1)
-            
-            
-            plt.figure()
-            plt.imshow(im)
-        
+    def predict(self, features):
+        if self.method == 'unsupervised':
+            model = self.__model
+            labels = model.predict(features)
+            if self.__speckle_class == 0:
+                labels = np.ones_like(labels) - labels
+        elif self.method == 'supervised':
+            model = self.__model
+            labels = model.predict(features)
+        else:
+            print('Model\'s method is not correct!')
         return labels
-        
-#####Fit model
-
-
-path = "E:/MathLab/EWI_Project/Database/Behnam/4C/original/"
-#path = "E:/MathLab/Python/Projects/Zaraban_project/source/track_object/test1/"
-
-im1 = Image.open(path + "im (2).bmp")
-im2 = Image.open(path + "im (5).bmp")
-
-
-im1 = im1.resize((200, 200))
-im2 = im2.resize((200, 200))
-
-im1 = np.array(im1)
-im2 = np.array(im2)
-f_size = im1.shape
-
-im_list = im1.reshape(1, f_size[0], f_size[1])
-im_list = np.concatenate((im_list, im2.reshape(1, f_size[0], f_size[1])), axis=0)
-
-
-samples_num = 200
-kernel_width = 5
-#samples_num*im_list.shape[0]
-
-speckle_modlel = Speckle()
-speckle_modlel.fit(im_list)
-
-#### Use Model
-X = np.random.randint(1+kernel_width, f_size[1]-kernel_width, samples_num)
-Y = np.random.randint(1+kernel_width, f_size[0]-kernel_width, samples_num)
-
-patches = np.zeros((samples_num, kernel_width*2+1, kernel_width*2+1))
-
-for num in range(1, samples_num):
-    patches[num, :, :] = im1[Y[num]-kernel_width:Y[num]+kernel_width+1, X[num]-kernel_width:X[num]+kernel_width+1]
-
-
-labels = speckle_modlel.predict(patches, visualize=True, image=im1)
-
-
-
+     
     
-
+    def visualize(self, points, labels, image=None, kernel_width=5):
+        
+        X = points[0]
+        Y = points[1]
+        speckles_x = X[labels==1]
+        speckles_y = Y[labels==1]     
+        hole_x = X[labels==0]
+        hole_y = Y[labels==0]
+        
+        im = image.copy()
+        for i in range(0, speckles_x.shape[0]):
+            imn = cv2.rectangle(im, (speckles_x[i]-kernel_width, speckles_y[i]-kernel_width)
+            , (speckles_x[i]+kernel_width, speckles_y[i]+kernel_width), (200, 200, 230), thickness=1)
+        
+        for i in range(0, hole_x.shape[0]):
+            imn = cv2.rectangle(im, (hole_x[i]-kernel_width, hole_y[i]-kernel_width)
+            , (hole_x[i]+kernel_width, hole_y[i]+kernel_width), (100, 100, 50), thickness=1)
+        
+        
+        plt.figure()
+        plt.imshow(im)
+        return 1
 
