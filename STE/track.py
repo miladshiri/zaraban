45a4af367516f9b7fae592dbdd7d2e529d464466
milad <miladonlinemail@gmail.com>
2019-04-18 13:51:07 +0430

    Add track_fixed_sequence, track_points_sequential, bug fix for track

--- STE/track.py
+++ STE/track.py
@@ -46,38 +46,47 @@ def track_fixed_points(frame1, frame2, WS, SS, model, show_message=False):
     progress = 0
     for row in range(1+PD, f_rows-PD):
         for col in range(1+PD, f_cols-PD):
-            progress += 1
-            print('{:.3}'.format(progress/((f_rows-2*PD)*(f_cols-2*PD))*100))
-            window = im1[row-WS:row+WS,col-WS:col+WS] 
-            if np.mean(window) > TH:
-                match_score = np.zeros((2*SS+1, 2*SS+1))
-                cross_col = np.zeros((2*SS+1, 2*SS+1))
-    
-                for ii in range(-SS, SS+1):
-                    for jj in range(-SS, SS+1):
-                        patch = im2[row+ii-WS:row+ii+WS, col+jj-WS:col+jj+WS]
-    #                    match_score[ii+SS, jj+SS] = np.sum(np.power(window-patch, 2))            
-                        cross_col[ii+SS, jj+SS] = abs(np.mean((window-window.mean())*(patch-patch.mean()) / (window.std()*patch.std())))
-                        match_score = cross_col
-#                        print (cross_col[ii+SS, jj+SS])
-                match_score = np.nan_to_num(match_score)
-                score_std[row, col] = match_score.std()
-                score_mean[row, col] = match_score.mean()
-                score_max[row, col] = match_score.max()
+            progress +=1
+            if show_message:
+                print('{:.3}'.format(progress/((f_rows-2*PD)*(f_cols-2*PD))*100))
+            window = frame1[row-WS:row+WS,col-WS:col+WS]
+#            if not np.mean(window) > 0:
+#                continue
+            feature = speckle.feature_extraction(window.reshape((1, window.shape[0], window.shape[1])))
+            label = model.predict(feature)
+            
+            if label[0] != 1: # If it is not speckle, go to next point.
+                continue
+            
+            match_score = np.zeros((2*SS+1, 2*SS+1))
+            cross_col = np.zeros((2*SS+1, 2*SS+1))
 
-                if match_score[SS, SS] == 1 or match_score[SS, SS] != match_score[SS, SS]:
-                       vectx[row, col] = 0;
-                       vecty[row, col] = 0; 
-                else:
-                    a, b = np.where(match_score == np.max(match_score))
-                    vectx[row, col] = a[0] - (SS)
-                    vecty[row, col] = b[0] - (SS)
+            for ii in range(-SS, SS+1):
+                for jj in range(-SS, SS+1):
+                    patch = frame2[row+ii-WS:row+ii+WS, col+jj-WS:col+jj+WS]
+#                    match_score[ii+SS, jj+SS] = np.sum(np.power(window-patch, 2))            
+                    if window.std()*patch.std():
+                        cross_col[ii+SS, jj+SS] = abs(np.mean((window-window.mean())*(patch-patch.mean()) / (window.std()*patch.std())))
+            match_score = cross_col
+#                        print (cross_col[ii+SS, jj+SS])
+            match_score = np.nan_to_num(match_score)
+            score_std[row, col] = match_score.std()
+            score_mean[row, col] = match_score.mean()
+            score_max[row, col] = match_score.max()
+
+            if match_score[SS, SS] == 1 or match_score[SS, SS] != match_score[SS, SS]:
+                vectx[row, col] = 0;
+                vecty[row, col] = 0; 
+            else:
+                a, b = np.where(match_score == np.max(match_score))
+                vectx[row, col] = a[0] - (SS)
+                vecty[row, col] = b[0] - (SS)
          
-    return vectx, vecty, score_std, score_mean, score_max
+    return vectx, vecty
     
 
         
-def track_point(frame1, frame2, markers, WS, SS):
+def track_specified_points(frame1, frame2, markers, WS, SS):
     ######
     ### track_point > applying block matching to track a point between 
     ###               two frames.
@@ -139,7 +148,33 @@ def track_point(frame1, frame2, markers, WS, SS):
      
     return (x_displacements + X, y_displacements + Y)
     
-               
+
+def track_points_sequential(frames, markers, kernel_radius, search_radius):
+    (oldX, oldY) = markers    
+    rows = frames.shape[0]
+    cols = oldX.shape[0]
+    all_new_x = np.zeros((rows, cols))
+    all_new_y = np.zeros((rows, cols))
+    all_new_x[0] = oldX.reshape((1, -1))
+    all_new_y[0] = oldY.reshape((1, -1))
+    
+    for i in range(0, rows-1):
+        newX, newY = track_specified_points(frames[i], frames[i+1],
+                     markers=(oldX, oldY), WS=kernel_radius, SS=search_radius)
+        (oldX, oldY) = (newX, newY)
+        all_new_x[i+1] = newX.reshape((1, -1))
+        all_new_y[i+1] = newY.reshape((1, -1))
+    return all_new_x, all_new_y
         
         
+def track_fixed_sequence(frames, WS, SS, model, show_message=True):
+    vectx = np.zeros(frames.shape)
+    vecty = np.zeros(frames.shape)
+    for i in range(0, frames.shape[0]-1):
+        (vectx[i+1], vecty[i+1]) = track_fixed_points(frame1=frames[i],
+        frame2=frames[i+1], WS=WS, SS=SS, model=model,
+        show_message=False)
+        if show_message:
+            print('{:.3}'.format((i+1)/(frames.shape[0]-1)*100))
+    return vectx, vecty
         
