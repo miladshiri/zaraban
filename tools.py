311ef0f05f2ca79871906dff65b6c8fd0444b435
milad <miladonlinemail@gmail.com>
2019-04-18 13:46:08 +0430

    Adding save_as_video and convert2map functions

--- tools.py
+++ tools.py
@@ -10,6 +10,7 @@ import numpy as np
 from PIL import Image
 from os import listdir
 from os.path import isfile, join
+import cv2
 
 def read_frames(path, size=(200, 200)):
     frames = np.array([])
@@ -26,3 +27,37 @@ def read_frames(path, size=(200, 200)):
         frame = frame.reshape(1, frame.shape[0], frame.shape[1])
         frames = np.concatenate((frames, frame), axis=0) if frames.size else frame
     return frames
+
+
+def save_as_video(path, frames):
+    if frames.ndim == 4:
+        frames = np.flip(frames, axis=3)
+    width = frames.shape[2]
+    height = frames.shape[1]
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(path, fourcc, 2, (width, height))
+    for frame in frames:
+        if frame.ndim < 3:
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+        out.write(frame)
+
+    out.release()
+    return 1
+
+
+def convert2map(v, search_radius):    
+    v = (v + search_radius) / (search_radius*2)
+    H = v * 180 * 250/360
+    S = np.ones_like(v) * 200
+    V = np.ones_like(v) * 160
+    vs = list(v.shape)
+    vs.append(3)
+    vhsv = np.ones(vs)
+    vhsv[:, :, :, 0] = H
+    vhsv[:, :, :, 1] = S
+    vhsv[:, :, :, 2] = V
+    vbgr = np.zeros(vhsv.shape, dtype=np.uint8)
+    for i in range(vhsv.shape[0]):
+        vbgr[i, :, :, :] = cv2.cvtColor(vhsv[i, :, :, :].astype(np.uint8), cv2.COLOR_HSV2BGR)
+    vrgb = np.flip(vbgr, axis=3)
+    return vrgb
